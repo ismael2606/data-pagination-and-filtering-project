68e6e65c3f21ad9097bc85d7f666ca159d824a2b
ismael2606 <ismaeldavid2010@gmail.com>
2025-03-23 18:12:14 -0700

    initialized showPage() func

--- js/script.js
+++ js/script.js
@@ -18,6 +18,21 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   let itemsPerPage = 9;
+   let startIndex = (page * itemsPerPage) - itemsPerPage; 
+   let endIndex = page * itemsPerPage;
+   const ul = document.getElementsByClassName('student-list');
+   ul.innerHTML = '';
+   // console.log(ul);
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         ul.insertAdjacenHTML('beforeend', "alo buenas");
+
+      }
+   }
+}
+
 
 
 /*
@@ -28,3 +43,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 4);
