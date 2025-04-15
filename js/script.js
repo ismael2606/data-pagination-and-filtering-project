4243c1fc8b24b8d0ed32340507e7d833c032e1f3
ismael2606 <ismaeldavid2010@gmail.com>
2025-04-14 23:04:59 -0400

    page done
    search feature in progress

--- js/script.js
+++ js/script.js
@@ -22,12 +22,21 @@ function showPage(list, page) {
    let itemsPerPage = 9;
    let startIndex = (page * itemsPerPage) - itemsPerPage; 
    let endIndex = page * itemsPerPage;
-   const ul = document.getElementsByClassName('student-list');
+   const ul = document.querySelector('.student-list');
    ul.innerHTML = '';
-   // console.log(ul);
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         ul.insertAdjacenHTML('beforeend', "alo buenas");
+         let studentCard = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+           <h3>${data[i].name.first} ${data[i].name.last}</h3>
+           <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${data[i].registered.date}</span>
+         </div>
+       </li>`
+         ul.insertAdjacentHTML('beforeend', studentCard);
 
       }
    }
@@ -40,7 +49,36 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+   const pageButtons = Math.ceil(list.length / 9);
+   const ul = document.querySelector('.link-list');
+   ul.innerHTML = '';
+   let buttons = '';
+
+   for (let i = 1; i <= pageButtons; i++) {
+      buttons += `<li><button type="button">${i}</button></li>`;
+   }
+   ul.insertAdjacentHTML('beforeend', buttons);
+
+
+   firstButton = document.querySelector('button').className = 'active';
+
+   ul.addEventListener('click', (e) => {
+      const activeButton = document.querySelector('button.active');
+      if (e.target.tagName === 'BUTTON') {
+         if (firstButton) {
+            activeButton.classList.remove('active');
+         }
+         e.target.classList.add('active');
+      }
+      showPage(list, e.target.innerHTML);
+   })
+}
+
+
+
 
 
 // Call functions
-showPage(data, 4);
+addPagination(data);
+showPage(data, 1);
